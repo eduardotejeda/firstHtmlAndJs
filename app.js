2102c697240ab80b226f799f755ce89d8080f54a
eduardotejedad <eduardotejedadelgado@gmail.com>
2022-10-09 07:25:26 -0400

    Add more logical operators

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-alert('Prueba');
+let respuesta = "3";
 
-if (1 === 1) {
+if (respuesta === 1) {
     console.log("It's true!");
+}
+else if (respuesta <= 1) {
+    console.log("It's false!");
+}
+else {
+    console.log("Invalid response.");
+}
+
+
+let age = 24;
+let gender = "male";
+if (age >= 18 && gender === "male"){
+    console.log("Can enter the competition");
+}
+else {
+    console.log("Can't enter the competition");
+}
+
+
+let helado = "Uva";
+if(!(helado === "fresa" && helado === "vainilla")){
+    console.log("NO TENEMOS ESE SABOR!")
 }
