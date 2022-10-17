03cfc18a0445676edefcd08c5f76c5e07ee14ffe
eduardotejedad <eduardotejedadelgado@gmail.com>
2022-10-17 01:21:28 -0400

    Fix valid password logic by my self :D

--- validpass.js
+++ validpass.js
@@ -2,7 +2,7 @@ function isValidPassword(username, password) {
 
     let passLength = password.length;
 
-    if(passLength >= 8 && password.includes(username)); {
+    if((passLength > 8) && (!password.includes(username) && (password.indexOf(' ') === -1))) {
         return true;
     } else {
         return false;
