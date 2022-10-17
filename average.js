f5931dd94e5f90ca7edf4fde8e6648ac8c7c67fa
eduardotejedad <eduardotejedadelgado@gmail.com>
2022-10-17 02:41:32 -0400

    Added average.js

---
+++ average.js
@@ -0,0 +1,12 @@
+function isAverage(num) {
+
+    let sumOfNumbers = 0
+for (let i = 0; i < num.length; index++) {
+       
+    sumOfNumbers += num[i];  
+    
+}
+let average = sumOfNumbers / num.length;
+console.log(average);
+
+}
