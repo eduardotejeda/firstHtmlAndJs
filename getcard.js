c267c5223ead325f3f0b3501e07a2a77feeb31ec
eduardotejedad <eduardotejedadelgado@gmail.com>
2022-10-21 05:38:38 -0400

    Update getcard with pick(arr) funtion

--- getcard.js
+++ getcard.js
@@ -1,14 +1,17 @@
+function pick(arr) {
+    const idx = Math.floor(Math.random() * arr.length);
+    return arr[idx];
+}
+
 function getCard() {
 
     const values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
-    const idx = Math.floor(Math.random() * values.length);
-
     const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
     
-    const suitsIdx = Math.floor(Math.random()* suits.length);
 
-console.log(values[idx], suits[suitsIdx]);
+    
+return Obj = {values : pick(values), suits : pick(suits)}
     
 }
 
