630bf0876ea2bb4a940e7ed0d94b6f738fdb4c15
eduardotejedad <eduardotejedadelgado@gmail.com>
2022-10-22 10:03:11 -0400

    Added functionsexpressions.js

---
+++ functionsexpressions.js
@@ -0,0 +1,21 @@
+/* Function expressions is another way, syntax, to define functions. Functions are objetcs too. */
+
+const square = function (num) {
+    return num * num;
+}
+
+// square(7); 49
+
+function add(x, y) {
+    return x + y;
+}
+
+const sum = function (x, y) { //anonymous function expression
+    return x + y; 
+}
+
+const suma = function inventario(x,y) { //named function expression
+    return x + y;
+}
+
+// The method console.dir() displays an interactive list of the properties of the specified JavaScript object.
