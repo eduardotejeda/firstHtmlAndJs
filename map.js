c641f218ee18b3147988ded522ed16d266be656f
eduardotejedad <eduardotejedadelgado@gmail.com>
2022-10-27 06:39:24 -0400

    Added map.js

---
+++ map.js
@@ -0,0 +1,9 @@
+// map() create a new array
+
+const numbers = [20,21,22,23,24,25,26,27];
+const words = ['asap','byop','rsvp','diy'];
+
+const doubles = numbers.map(function(num) {
+    return num*2;
+});
+
