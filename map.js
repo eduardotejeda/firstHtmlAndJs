b670690c8c384e3fb78f91c47035462ebd6d50ac
eduardotejedad <eduardotejedadelgado@gmail.com>
2022-10-28 06:41:37 -0400

    remove map.js

--- map.js
+++
@@ -1,9 +0,0 @@
-// map() create a new array
-
-const numbers = [20,21,22,23,24,25,26,27];
-const words = ['asap','byop','rsvp','diy'];
-
-const doubles = numbers.map(function(num) {
-    return num*2;
-});
-
