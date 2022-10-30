c9153ac9abc8478eac8c744f0c8453271744aa8e
eduardotejedad <eduardotejedadelgado@gmail.com>
2022-10-30 06:50:01 -0400

    Adden arrowFunctions.js

--- arrowFunction.js
+++ arrowFunction.js
@@ -4,4 +4,33 @@ const square = (x) => {
 
 const isEven = (num) => {
     return num % 2 === 0;
+
 }
+
+const multyply = (x,y) => {
+    return x * y;
+}
+
+const greet = () => {
+    console.log('HI!!')
+}
+
+//Arrow functions implicit return
+
+//const square2 = n => (
+//    n * n
+//)
+
+//const square2 = n => n * n;
+
+const nums = [1,2,3,4,5,6,7,8,9];
+
+const doubles1 = nums.map(function (n) {
+    return n * 2;
+})
+
+const doubles2 = nums.map(n => {
+    return n * 2;
+})
+
+const doubles3 = nums.map(n => n * 2);
