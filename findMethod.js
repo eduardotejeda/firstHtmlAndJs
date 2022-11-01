f5b8e37de5fabbcf73fc4b7d0627dca67c526b60
eduardotejedad <eduardotejedadelgado@gmail.com>
2022-11-01 04:50:07 -0400

    Added findMethod.js

---
+++ findMethod.js
@@ -0,0 +1,44 @@
+let movies = [
+    "The Fantastic Mr. fox",
+    "Mr. and Mrs. Smith",
+    "Mrs. Doubtfire",
+    "Mr. Deeds"
+]
+
+const movie = movies.find(movie => {
+    return movie.includes('Mrs');
+})
+
+const movie2 = movies.find(m => (
+    m.indexOf("Mrs") === 0
+))
+
+
+const books = [
+    {
+        title:'Good Omes',
+        authors:['Terry Pratchett','Neil Gaiman'],
+        rating:4.25
+    },
+    {
+        title: 'Bone: The Complete Edition',
+        authors:['Jeff Smith'],
+        rating:4.11
+    },
+    {
+    title: 'American Gods',
+    authors:['Neil Gaiman'],
+    rating:4.42
+    },
+    {
+        title: 'A Gentleman in Moscow',
+        authors:['Amor Towles'],
+        rating: 4.36
+    }
+    ]
+
+    
+const goodBook = books.find(b => b.rating >= 4.3);
+const neilBook = books.find(b => (
+    b.authors.includes('Neil Gaiman')
+))
