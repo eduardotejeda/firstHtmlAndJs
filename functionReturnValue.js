f29847d8d7b902c9aec86a4b49ec1882bde27dbc
eduardotejedad <eduardotejedadelgado@gmail.com>
2022-11-06 12:31:29 -0400

    Added more examples to functionReturnValue.js

--- functionReturnValue.js
+++ functionReturnValue.js
@@ -1,11 +1,50 @@
-function confirmaAno(year) {
-    return function(x,y){
-        return year >= x && year <= y;
-    }
+//function confirmaAno(year) {
+ //   return function(x,y){
+ //       return year >= x && year <= y;
+ //   }
     
-}
+//}
 
-const anodeano = confirmaAno(1912);
+//const anodeano = confirmaAno(1912);
 //const years =  function(x,y);
 
- 
+function doble(x) {
+    return function(num) {
+        return x * num;
+    }
+}
+
+const duplicar = doble(2)
+
+///////////////
+
+function yearToCheck(year) {
+    return function (x, y) {
+        return year >= x && year <= y;
+    }
+   
+}
+
+const checkYears = yearToCheck(1912);
+
+const isChild = yearToCheck(14);
+
+const isNiceWeather = yearToCheck(23);
+
+///////
+
+function checkTolerance(a,b) {
+    return function(cifra) {
+        return cifra >= a && cifra <= b;
+    }
+}
+
+const checkTemp = checkTolerance(80,196);
+const checkAge = checkTolerance(18,67);
+const checkWeather = checkTolerance(15,26);
+
+
+let textTest = function(text) {
+    return text;
+}
+
--- reduce.js
+++ reduce.js
@@ -10,3 +10,26 @@ const maxGrade = grades.reduce((max, currVal) => {
     if (currVal > max) return currVal;
     return max;
 })
+
+const nums2 = [10,20, 30, 40, 50].reduce((sum, num) => {
+    return sum + num;
+}, 100) //.reduce tiene la opcion de iniciar con un numero difirente agregando despues del }, numero.
+
+//Tallying or frecuency counter to make objects
+
+const votes = ['y', 'y', 'n', 'y', 'absent', 'n', 'y','y','n','n'];
+
+const results = votes.reduce((tally, val) => {
+if (tally[val]) {
+    tally[val]++
+} else {
+    tally[val] =1;
+}
+return tally;
+}, {})
+
+ 
+const results2 = votes.reduce((tally, val) => {
+    tally[val] = (tally[val] || 0) + 1;
+    return tally;
+}, {}); 
